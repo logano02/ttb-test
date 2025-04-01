8a4abdcf78713f0a58ee1d2dc1d79c06b66a26d9
logano02 <monthon049@gmail.com>
2025-04-01 14:59:44 +0700

    set default

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Confirm from "./pages/confirm";
 import { Outlet } from "@mui/icons-material";
 import { Box } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Box width={'100%'}>
+      <Outlet />
+      <Box width={"100%"}>
         <Confirm />
       </Box>
     </>
