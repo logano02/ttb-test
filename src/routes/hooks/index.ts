849e3f0adeb20f6d0d73a91857f4706bd6ca8632
logano02 <monthon049@gmail.com>
2025-04-03 01:58:45 +0700

    remove not use folder

--- src/routes/hooks/index.ts
+++ src/routes/hooks/index.ts
@@ -1,10 +1,7 @@
-export * from './use-active-link';
-//
+export { useParams } from "./use-params";
 
-export { useParams } from './use-params';
+export { useRouter } from "./use-router";
 
-export { useRouter } from './use-router';
+export { usePathname } from "./use-pathname";
 
-export { usePathname } from './use-pathname';
-
-export { useSearchParams } from './use-search-params';
+export { useSearchParams } from "./use-search-params";
